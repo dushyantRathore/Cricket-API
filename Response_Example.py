96f8365c9832bec99a48dadb442597f14ba86176
dushyantRathore <dushyant.bgs@gmail.com>
2017-02-13 12:09:24 +0530

    Response code example

---
+++ Response_Example.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+url = "https://powerful-tor-13817.herokuapp.com/news"
+
+headers = {
+    'cache-control': "no-cache",
+    'postman-token': "e4db8c8c-1774-f923-3400-5f8b29be5bde"
+    }
+
+response = requests.request("POST", url, headers=headers)
+
+news = response.text
+
+json_object = json.loads(news)
+
+print json_object["Latest News"][1]["Title"]
+
+
+
+
+
